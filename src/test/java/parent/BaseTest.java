3322ff6c0473134ad2210719914b52bb548e3575
Kate-sad <kate.sky24@mail.ru>
2025-09-23 20:59:22 +0300

    Commit LoginTest and BaseTest

---
+++ src/test/java/parent/BaseTest.java
@@ -0,0 +1,28 @@
+package parent;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+
+public class BaseTest {
+    public WebDriver browser;
+
+   @BeforeMethod
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("--guest");
+        browser = new ChromeDriver(options);
+        browser.get("https://www.saucedemo.com/");
+    }
+
+    @AfterMethod
+    public void close() {
+       browser.quit();
+    }
+
+
+}
